aa3913d392bb9566e9e62daaa06a386e4709041a
SuhyeonB <peridot489@gmail.com>
2026-08-11 11:25:45 +0900

    initial commit

---
+++ src/main/java/com/example/goodsrush/GoodsrushApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodsrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsrushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goodsrush/GoodsrushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodsrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsrushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
